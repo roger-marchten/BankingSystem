5b939badf88e642309e4d47c41d7f2a1cb98146c
roger <roger.marchten0211@gmail.com>
2024-12-23 12:22:22 +0200

    Hi

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,79 @@
 public class Main {
     public static void main(String[] args) {
-        // Create the bank
-        Bank bank = new Bank();
+        //Create Standard Account with credit limit of -100
+        iAccounts standard = new StandardAccount(1,-100);
+        //Create Basic Account with Withdraw limit of 100
+        iAccounts basic = new BasicAccount(2,100);
+        //Create new Premium Account
+        iAccounts premium = new PremiumAccount(3);
+        //Create new bank
+        iBank bank = new Bank();
 
-        // Open accounts
-        StandardAccount standardAccount = new StandardAccount(1, -100);
-        BasicAccount basicAccount = new BasicAccount(2, 50);
-        PremiumAccount premiumAccount = new PremiumAccount(3);
+        //Open those 3 accounts in the bank
+        bank.OpenAccount(standard);
+        bank.OpenAccount(premium);
+        bank.OpenAccount(basic);
 
-        bank.OpenAccount(standardAccount);
-        bank.OpenAccount(basicAccount);
-        bank.OpenAccount(premiumAccount);
+        //Deposit 1000 in each account
+        System.out.println("Depositing 1000 in each account...");
+        premium.deposit(1000);
+        standard.deposit(1000);
+        basic.deposit(1000);
+        System.out.println("");
 
-        // Test deposits
-        standardAccount.deposit(200);
-        basicAccount.deposit(100);
-        premiumAccount.deposit(500);
+        System.out.println("Withdrawing 3000 from each account...");
+        //Try to withdraw 3000 from the premium account (should print 3000)
+        System.out.println("Withdrawn " + premium.withdraw(3000) + " From Premium Account");
+        //Try to withdraw 3000 from the Basic account (should print 100)
+        System.out.println("Withdrawn " + basic.withdraw(3000) + " From Basic Account");
+        //Try to withdraw 3000 from the Standard account (should print 1100)
+        System.out.println("Withdrawn " + standard.withdraw(3000) + " From Standard Account");
+        System.out.println("");
 
-        System.out.println("Standard Account Balance: " + standardAccount.getCurrentBalance()); // Should be 200
-        System.out.println("Basic Account Balance: " + basicAccount.getCurrentBalance()); // Should be 100
-        System.out.println("Premium Account Balance: " + premiumAccount.getCurrentBalance()); // Should be 500
+        //Check Balance in Premium Account (Should print -2000)
+        System.out.println("Current Balance in account number " + premium.getAccountNumber()+ "(Premium) is " + premium.getCurrentBalance());
+        //Check Balance in Basic Account (Should print 900)
+        System.out.println("Current Balance in account number " + basic.getAccountNumber()+ "(Basic) is " + basic.getCurrentBalance());
+        //Check Balance in Standard Account (Should print -100)
+        System.out.println("Current Balance in account number " + standard.getAccountNumber()+ "(Standard) is " + standard.getCurrentBalance());
+        System.out.println("");
 
-        // Test withdrawals
-        double standardWithdraw = standardAccount.withdraw(350); // Should withdraw 300 (200 balance + 100 credit limit)
-        double basicWithdraw = basicAccount.withdraw(70); // Should withdraw 50 (withdrawal limit)
-        double premiumWithdraw = premiumAccount.withdraw(600); // Should withdraw 600 (no limit)
+        //Print all accounts in debt (should print accounts 1 and 3)
+        for (iAccounts account: bank.GetAllAccountsInDebt() ) {
+            System.out.println("Account " + account.getAccountNumber() + " is in debt");
+        }
+        System.out.println("");
 
-        System.out.println("Standard Account Withdrawn: " + standardWithdraw);
-        System.out.println("Basic Account Withdrawn: " + basicWithdraw);
-        System.out.println("Premium Account Withdrawn: " + premiumWithdraw);
+        //Close All Bank Accounts
+        System.out.println("Closing all accounts in the bank...");
+        bank.CloseAccount(premium.getAccountNumber());
+        bank.CloseAccount(basic.getAccountNumber());
+        bank.CloseAccount(standard.getAccountNumber());
+        //check how many accounts are present in the bank (should be only 2 - cannot close accounts in debt)
+        System.out.println("There are " + bank.GetAllAccounts().size() + " accounts in the bank");
+        System.out.println("");
 
-        System.out.println("Standard Account Balance After Withdrawal: " + standardAccount.getCurrentBalance());
-        System.out.println("Basic Account Balance After Withdrawal: " + basicAccount.getCurrentBalance());
-        System.out.println("Premium Account Balance After Withdrawal: " + premiumAccount.getCurrentBalance());
+        //Deposit 2000 to each of the accounts left
+        System.out.println("Depositing 2000 in each account...");
+        premium.deposit(2000);
+        standard.deposit(2000);
 
-        // Test Bank methods
-        System.out.println("\n--- Bank Operations ---");
+        //Print all accounts with balance more than 1000 (should print account 1 only)
+        for (iAccounts account: bank.GetAllAccountsWithBalance(1000) ) {
+            System.out.println("Account " + account.getAccountNumber() + " has more than 1000 in its balance");
+        }
+        System.out.println("");
 
-        // Get all accounts
-        System.out.println("Total Accounts in Bank: " + bank.GetAllAccounts().size()); // Should be 3
+        //Check Balance in Premium Account (Should print 0)
+        System.out.println("Current Balance in account number " + premium.getAccountNumber()+ "(Premium) is " + premium.getCurrentBalance());
+        //Check Balance in Standard Account (Should print 1900)
+        System.out.println("Current Balance in account number " + standard.getAccountNumber()+ "(Standard) is " + standard.getCurrentBalance());
 
-        // Get accounts in debt
-        System.out.println("Accounts in Debt: " + bank.GetAllAccountsInDebt().size()); // StandardAccount should be in debt
-
-        // Get accounts with balance > 50
-        System.out.println("Accounts with Balance > 50: " + bank.GetAllAccountsWithBalance(50).size()); // Should be 2 (Basic and Premium)
-
-        // Try closing accounts
-        System.out.println("\nAttempting to Close Accounts:");
-        bank.CloseAccount(1); // Should print: Account cannot be closed due to debt
-        bank.CloseAccount(2); // Should close successfully
-        System.out.println("Total Accounts in Bank After Closure: " + bank.GetAllAccounts().size()); // Should be 2
-    }
-}
+        //Close All Bank Accounts
+        System.out.println("");
+        System.out.println("Closing all bank accounts...");
+        bank.CloseAccount(premium.getAccountNumber());
+        bank.CloseAccount(standard.getAccountNumber());
+        //check how many accounts are present in the bank (should be only 0)
+        System.out.println("There are " + bank.GetAllAccounts().size() + " accounts in the bank");
+    }}
